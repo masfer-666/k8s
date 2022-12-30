74b4b8e8627d18422e8b873f42373034a5aefe4d
MasFer <34679134+masfer-666@users.noreply.github.com>
2022-12-30 18:59:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt update && apt upgrade -y
 
 # install package dependencies
 apt-get install net-tools git traceroute mtr vim bind9-utils rsync htop wget nmap -y
-EOF
+
 # ansible
 sudo apt-add-repository ppa:ansible/ansible && sudo apt update && sudo apt install ansible -y 
 
@@ -13,6 +13,7 @@ sudo apt-add-repository ppa:ansible/ansible && sudo apt update && sudo apt insta
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config && service sshd restart
 # create rsa keys
 ssh-keygen -t rsa 
+passwd root
 ssh-copy-id root@10.2.16.152
 ssh-copy-id root@10.2.16.153
 ssh-copy-id root@10.2.16.154
